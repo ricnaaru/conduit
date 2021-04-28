e2b2cda8d2fe6ed24c50c2cca45451c802b0efa6
Brett Sutton <bsutton@noojee.com.au>
2021-04-28 19:31:13 +1000

    Switch to using the critical_test package to run unit tests.

--- conduit/tool/run_unit_tests.dart
+++ conduit/tool/run_unit_tests.dart
@@ -4,9 +4,6 @@ import 'dart:io';
 
 import 'package:dcli/dcli.dart';
 
-import 'db_settings.dart';
-import 'postgres_manager.dart';
-
 /// This script will run the conduit unit tests
 ///
 /// To run this script install dcli:
@@ -21,62 +18,10 @@ import 'postgres_manager.dart';
 /// ```
 ///
 void main(List<String> args) {
-  if (which('psql').notfound) {
-    printerr(red(
-        'Postgres not found. Have you run "install_unit_test_dependencies.dart".'));
+  if (which('critical_test').notfound) {
+    print('Installing global package critical_test');
+    DartSdk().globalActivate('critical_test');
     exit(1);
   }
-
-  ProcessSignal.sigint.watch().listen((signal) {
-    print('ctrl-caught');
-    'docker-compose down'.run;
-  });
-
-  final projectRoot = DartProject.current.pathToProjectRoot;
-  final pathToToolDir = join(projectRoot, 'tool');
-  DartSdk().runPubGet(projectRoot);
-
-  var dbSettings = DbSettings.load(pathToSettings: pathToToolDir);
-  dbSettings.createEnvironmentVariables();
-
-  var postgresManager = PostgresManager(dbSettings);
-
-  if (dbSettings.useContainer) {
-    print('Starting postgres docker image');
-    postgresManager.startPostgresDaemon(pathToToolDir);
-  }
-
-  postgresManager.waitForPostgresToStart();
-
-  print('recreating database');
-  postgresManager.dropPostgresDb();
-  postgresManager.dropUser();
-
-  postgresManager.createPostgresDb();
-
-  print(red('Activating local copy of conduit.'));
-  // print(orange('You may want to revert this after the unit tests finish!'));
-
-  // activate('common');
-  // activate('conduit-config');
-  // activate('isolate-exec');
-  // activate('open-api-dart');
-  // activate('password-hash');
-  // activate('conduit-runtime');
-  // activate('common_test');
-
-  'pub global activate . --source=path'.start(workingDirectory: projectRoot);
-
-  print('Starting Conduit unit tests');
-
-  /// run the tests
-  'pub run test -j1'.start(workingDirectory: projectRoot);
-
-  print('Stopping posgress docker image');
-  postgresManager.stopPostgresDaemon(pathToToolDir);
+  'critical_test'.run;
 }
-
-// void activate(String projectRoot,  package) {
-//   'pub global activate $package --source=path'.start(
-//       workingDirectory: truepath(projectRoot, '..'));
-// }
