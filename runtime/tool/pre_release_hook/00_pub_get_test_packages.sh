3b517d32482eee1c9b22402fcde4bfe33a5675db
Brett Sutton <bsutton@noojee.com.au>
2021-06-02 17:05:03 +1000

    Fixed the test paths to point to the new home of runtime_test_packages

--- runtime/tool/pre_release_hook/00_pub_get_test_packages.sh
+++ runtime/tool/pre_release_hook/00_pub_get_test_packages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pushd test/test_packages/dependency
+pushd ../runtime_test_packages/dependency
 dart pub get
 popd
 
-pushd test/test_packages/application
+pushd ../runtime_test_packages/application
 dart pub get
 popd
