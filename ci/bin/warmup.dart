a88865d0a46985c08461f52356aea17d1b8c1680
Brett Sutton <bsutton@noojee.com.au>
2021-06-02 17:06:15 +1000

    add arg to warmup to allow an upgrade of packages.

--- ci/bin/warmup.dart
+++ ci/bin/warmup.dart
@@ -6,12 +6,23 @@ import 'package:dcli/dcli.dart';
 /// Use this method after cloning the repo to
 /// ensure that all dependencies have been
 /// downloaded
-void main() {
+void main(List<String> args) {
+  var parser = ArgParser();
+  parser.addFlag('upgrade',
+      defaultsTo: false, help: 'Upgrades all of the project dependencies');
+
+  final parsed = parser.parse(args);
+
+  final upgrade = parsed['upgrade'] as bool;
+
   final project = DartProject.fromPath('.');
+
+  final action = (upgrade) ? 'upgrade' : 'get';
 
   find('pubspec.yaml', workingDirectory: dirname(project.pathToProjectRoot))
       .forEach((pubspec) {
     print('Running pub get for ${dirname(pubspec)}');
-    DartSdk().runPubGet(dirname(pubspec), progress: Progress.printStdErr());
+    DartSdk().runPub(
+        args: [action, dirname(pubspec)], progress: Progress.printStdErr());
   });
 }
